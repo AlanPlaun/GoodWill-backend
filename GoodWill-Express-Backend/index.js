5bf719d86d74bcf6a5288a971e7862108dc7d8c0
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-15 10:43:23 -0300

    empieza la coneccion

--- GoodWill-Express-Backend/index.js
+++ GoodWill-Express-Backend/index.js
@@ -2,16 +2,14 @@
 import config  from './dbconfig-env.js';
 import usuario from './src/models/usuario.js';
 import UsuarioServices from "./src/services/usuario-services.js";
-// import express from 'express';
 //requiere cors para poder ser accedida desde el front
-
 import  Express  from "express";
 const app = Express();
-app.use(cors())
+//app.use(cors())
 const port = 5000;
-
-app.get('/usuarios', async (req,res) =>{
-    const user = new UsuarioServices();
+//recibe la informacion
+app.get('/ingresosesion', async (req,res) =>{
+    const MailContraseña = req.body;
     let data = await user.GetAll();
     res.send(data);
 })
